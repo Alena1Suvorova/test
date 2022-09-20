4873718aff11b8070fafae71d0009426f4d8fc56
Alena Suvorova <suvorova.alena26@gmail.com>
2022-09-20 17:06:50 +0300

    changed main

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-A = int(input())
-B = int(input())
-H = int(input())
-
-if H>=A and H<=B:
-    print('Это нормально')
-elif H<A:
-    print('Недосып')
+n = int(input())
+if (n%4==0 and n%100!=0) or n % 400==0:
+    print('Високосный')
 else:
-    print('Пересып')
-
+    print('Обычный')
