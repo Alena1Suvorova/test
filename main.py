0264bce89bd32e6ad29c2e9c6076a9eefd8b61e6
Alena Suvorova <suvorova.alena26@gmail.com>
2022-09-20 16:33:01 +0300

    changed main

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hi,Alena")
+A = int(input())
+B = int(input())
+H = int(input())
+
+if H>=A and H<=B:
+    print('Это нормально')
+elif H<A:
+    print('Недосып')
+else:
+    print('Пересып')
+
