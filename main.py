6660418bcdc7cde3f9f5ea94f383f9d4ca38934d
Alena Suvorova <suvorova.alena26@gmail.com>
2022-09-20 22:46:24 +0300

    changed main

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
-a= int(input())
-if (a>-15 and a<=12) or (a>14 and a<17) or a>=19:
-    print('True')
+a=float(input())
+b=float(input())
+c=input()
+if c =='+':
+  print(a+b)
+elif c =='-':
+    print(a-b)
+elif c=='*':
+    print(a*b)
+elif c=='/':
+    if b!=0 :
+        print(a/b)
+    else: print('Деление на 0!')
+elif c=='mod':
+   if b!=0 :
+       print(a%b)
+   else:  print('Деление на 0!')
+elif c=='pow':
+    print(a**b)
+elif c=='div':
+    if b != 0:
+        print(a//b)
+    else:
+        print('Деление на 0!')
 else:
-    print('False')
+    print('нет такой операции')
+
