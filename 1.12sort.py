76a9b3edb0514dc35f9ddd451dc13727792d5d4a
Alena Suvorova <suvorova.alena26@gmail.com>
2022-09-25 20:42:49 +0300

    bilet

--- 1.12sort.py
+++ 1.12sort.py
@@ -47,6 +47,14 @@ elif (c>=a and c<=b) or (c>=b and c<=a):
         print(a, 'программиста')
     else:
         print(a, 'программистов')
+        # счастливый билет
+        a = str(input())
+        b = int(a[0]) + int(a[1]) + int(a[2])
+        c = int(a[3]) + int(a[4]) + int(a[5])
+        if b == c:
+            print('Счастливый')
+        else:
+            print('Обычный')
 
 
 
