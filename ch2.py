8f73d8f53414224ace7a8f594f565db44f1248cc
Alena Suvorova <suvorova.alena26@gmail.com>
2022-09-29 15:35:34 +0300

    l2.1 2

--- ch2.py
+++ ch2.py
@@ -1,7 +1,19 @@
-#2.1
-s=0
-a=1
-while a !=0:
+# 2.1
+
+s = 0
+a = 1
+
+while a != 0:
     a = int(input())
-    s+=a
+    s += a
+
 print(int(s))
+
+#2.1 2
+a = int(input())
+b = int(input())
+d = 1
+while (d % a != 0) or (d % b != 0):
+    d += 1
+
+print(d)
