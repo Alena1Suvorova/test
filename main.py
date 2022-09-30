dbaeb4add0707ba0d3b3ec1a9ab9c6da2a2154cf
Alena Suvorova <suvorova.alena26@gmail.com>
2022-09-30 14:48:23 +0300

    l2.2

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
-h=input()
-if h=='треугольник':
-    a = float(input())
-    b = float(input())
-    c = float(input())
-    p=(a+b+c)/2
-    S=(p*(p-a)*(p-b)*(p-c))**0.5
-    print(S)
-elif h=='прямоугольник':
-    a = float(input())
-    b = float(input())
-    S = a * b
-    print(S)
-elif h=='круг':
-    r = float(input())
-    S = 3.14*(r**2)
-    print(S)
-else:
-    print()
+a = 7
+b = 10
+c = 5
+d = 6
+for j in range(c, d+1):
+    print('\t', j, end='')
+for i in range(a, b+1):
+    print('\n')
+    print(i, end='')
+    for j in range(c, d+1):
+        print('\t', i * j, end='')
+
+
+
+
+
+
+
+
+
+
+
 
 
