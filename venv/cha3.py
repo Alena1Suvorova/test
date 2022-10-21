525ef0a1cc0f614bb71bcf2ff31f987bfcfabca0
Alena Suvorova <suvorova.alena26@gmail.com>
2022-10-21 23:04:37 +0300

    3funk

--- venv/cha3.py
+++ venv/cha3.py
@@ -11,10 +11,12 @@ def fan(x):
     return s
 # 3.1.2
 lst = [1, 2, 3, 4, 5, 6]
+
 def modify_list(l):
-    for j in range(len(l)-1,-1,-1):
-        if l[j]%2 != 0:
-           l.pop(j)
+    for j in range(len(l)-1, -1, -1):
+        if l[j] % 2 != 0:
+            l.pop(j)
         else:
-            l[j]=l[j]//2
+            l[j] = l[j]//2
+
 modify_list(lst)
