a8b6db30ad924af867b7160c8623edbb0a1bff8d
Alena Suvorova <suvorova.alena26@gmail.com>
2022-10-21 23:02:19 +0300

    l2.6.6_matr

--- venv/cha3.py
+++ venv/cha3.py
@@ -1,4 +1,5 @@
-# 3.1
+#3 функции
+# 3.1.1
 def fan(x):
     s = 0
     if x <= -2:
@@ -8,3 +9,12 @@ def fan(x):
     elif x > 2:
         s = ((x - 2) ** 2) + 1
     return s
+# 3.1.2
+lst = [1, 2, 3, 4, 5, 6]
+def modify_list(l):
+    for j in range(len(l)-1,-1,-1):
+        if l[j]%2 != 0:
+           l.pop(j)
+        else:
+            l[j]=l[j]//2
+modify_list(lst)
